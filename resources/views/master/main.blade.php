9ed384d7c510e68770f854d2d34c920462c06b48
mohfikryansyah <moh.fikryansyah@gmail.com>
2023-12-13 10:36:46 +0800

    korupsi

--- resources/views/master/main.blade.php
+++ resources/views/master/main.blade.php
@@ -31,6 +31,15 @@
                         </svg>
                         Gratifikasi
                     </p>
+                    <p
+                        class="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 mb-2">
+                        <svg class="w-2.5 h-2.5 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor" viewBox="0 0 18 18">
+                            <path
+                                d="M17 11h-2.722L8 17.278a5.512 5.512 0 0 1-.9.722H17a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1ZM6 0H1a1 1 0 0 0-1 1v13.5a3.5 3.5 0 1 0 7 0V1a1 1 0 0 0-1-1ZM3.5 15.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM16.132 4.9 12.6 1.368a1 1 0 0 0-1.414 0L9 3.55v9.9l7.132-7.132a1 1 0 0 0 0-1.418Z" />
+                        </svg>
+                        Korupsi
+                    </p>
                     <h2 class="text-gray-900 dark:text-white text-4xl font-extrabold mb-2">Selamat Datang ...
                     </h2>
                     <p class="text-xl font-normal text-gray-500 dark:text-gray-400 mb-4">Website ini diciptakan sebagai
