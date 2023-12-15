df54c627bf288c26dbf9f0ad48114abdb2cd5b7c
mohfikryansyah <moh.fikryansyah@gmail.com>
2023-12-15 08:27:30 +0800

    x

--- resources/views/master/layouts/dashboard-tabel.blade.php
+++ resources/views/master/layouts/dashboard-tabel.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
     <div class="px-5">
-        <div class="pt-3 flex flex-column sm:flex-row flex-wrap space-y-4 md:space-y-0 items-center justify-between pb-4 overflow-hidden">
+        <div
+            class="pt-3 flex flex-column sm:flex-row flex-wrap space-y-4 md:space-y-0 items-center justify-between pb-4 overflow-hidden">
             <div class="w-full md:w-auto md:inline-flex md:space-x-4 md:space-y-0 space-y-2">
                 <div class="bg-yellow-400 max-w-sm text-gray-100 rounded-lg px-3 py-1.5">
                     <h1 class="font-bold text-center">{{ $pengaduan->where('kategori', 'Pungutan Liar')->count() }} Laporan
@@ -84,10 +85,16 @@
                                         <span class="w-2 h-2 mr-1 bg-green-500 rounded-2xl"></span>
                                         {{ $data->status }}
                                     </span>
+                                @elseif ($data->status == 'Sedang diproses')
+                                    <span
+                                        class="inline-flex items-center bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300">
+                                        <span class="w-2 h-2 mr-1 bg-red-500 rounded-2xl"></span>
+                                        {{ $data->status }}
+                                    </span>
                                 @else
                                     <span
-                                        class="inline-flex items-center bg-red-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">
-                                        <span class="w-2 h-2 mr-1 bg-red-500 rounded-2xl"></span>
+                                        class="inline-flex items-center bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">
+                                        <span class="w-2 h-2 mr-1 bg-blue-500 rounded-2xl"></span>
                                         {{ $data->status }}
                                     </span>
                                 @endif
@@ -109,13 +116,13 @@
                                     target="_blank">Lihat</a>
                             </td>
                             <td class="px-6 py-4">
-                                {{-- <form action="/dashboard/{{ $data->id }}" method="POST">
-                                    @csrf
-                                    @method('put')
-                                    <button class=" text-blue-500 font-semibold">Confirm</button>
-                                </form> --}}
-
-                                <a href="{{ route('sendEmail', ['id' => $data->id]) }}">Kirim</a>
+                                @if ($data->status == 'Baru')
+                                    <a href="{{ route('sendEmail', ['id' => $data->id]) }}"
+                                        class="text-blue-500">Proses</a>
+                                @elseif ($data->status == 'Sedang diproses')
+                                    <a href="{{ route('sendEmail', ['id' => $data->id]) }}"
+                                        class="text-blue-500">Selesaikan</a>
+                                @endif
                             </td>
                         </tr>
                     @empty
